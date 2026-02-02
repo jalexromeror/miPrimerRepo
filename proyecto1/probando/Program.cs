5dff4a503c8e30a46e55d830511abc092ab7853e
Yimy Romero <jalexromeror@gmail.com>
2026-02-02 17:40:28 -0500

    comentario api key

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("¡Hola, mundo!");
 
-string stripe_api_key="sk_teste_4eC39HqLyjWDarjtT1zdp7dc"
+// string stripe_api_key="sk_teste_4eC39HqLyjWDarjtT1zdp7dc"
